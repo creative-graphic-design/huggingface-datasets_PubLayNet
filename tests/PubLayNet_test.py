309489ce2ba762e3331683c69f6a2ba5d2c36e48
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2023-11-09 14:57:14 +0900

    update test

--- tests/PubLayNet_test.py
+++ tests/PubLayNet_test.py
@@ -1,3 +1,5 @@
+import os
+
 import datasets as ds
 import pytest
 
@@ -7,12 +9,16 @@ def dataset_path() -> str:
     return "PubLayNet.py"
 
 
-@pytest.mark.parametrize(
-    argnames="decode_rle",
-    argvalues=(
-        False,
-        True,
+@pytest.mark.skipif(
+    condition=bool(os.environ.get("CI", False)),
+    reason=(
+        "Because this loading script downloads a large dataset, "
+        "we will skip running it on CI."
     ),
+)
+@pytest.mark.parametrize(
+    argnames=("decode_rle"),
+    argvalues=(False, True),
 )
 def test_load_dataset(dataset_path: str, decode_rle: bool):
     dataset = ds.load_dataset(path=dataset_path, decode_rle=decode_rle)
